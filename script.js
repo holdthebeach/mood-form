80f80be6d11a75b20b37d2cf623811742cfb73c2
Patricia <holdthebeach@gmail.com>
2015-01-15 17:56:36 +0000

    tried to add jquery to send data from form

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-$
+$(document).ready(function(){
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        $.ajax('/record', {
+            type: 'POST',
+            data: $('form').serialize(),
+            success: function(result){
+                this.closest('#inputStatement').find('#confirmation').show();
+            }
+        });
+    });
+    });
